fc0f8837e9818a54733c681538ace61e23733a83
JonNixonCodes <jonathan.yu.94@gmail.com>
2025-04-10 04:38:25 +1000

    Refreshed data

--- src/bigquery/ext_processed_features.sql
+++ src/bigquery/ext_processed_features.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE EXTERNAL TABLE `leverageai-sandbox.source.ext_processed_features`
 OPTIONS (
   format = 'PARQUET',
-  uris = ['gs://leverageai-sandbox-data/processed/features/20250406_162326/features.parquet/*.parquet']
+  uris = ['gs://leverageai-sandbox-data/processed/features/20250410_035450/features.parquet/*.parquet']
 );
--- src/bigquery/ext_processed_predictions.sql
+++ src/bigquery/ext_processed_predictions.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE EXTERNAL TABLE `leverageai-sandbox.source.ext_processed_predictions`
 OPTIONS (
   format = 'PARQUET',
-  uris = ['gs://leverageai-sandbox-data/processed/predictions/20250406_163428/predictions.parquet']
+  uris = ['gs://leverageai-sandbox-data/processed/predictions/20250410_035725/predictions.parquet']
 );
